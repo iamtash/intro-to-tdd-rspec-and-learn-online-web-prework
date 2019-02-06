fa4b99410eb89887a12454d6762c1d078d6642ce
Natasha Chernis <natashachernis@gmail.com>
2019-02-06 05:07:14 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -2,4 +2,4 @@ require relative './current_age_for_birth_year.rb'
 puts "What year were you born?"
 birth_year = gets.to_i
 users_age = current_age_for_birth_year(birth_year)
-puts "You are " users_age.to_s + "years old."
+puts "You are " + users_age.to_s + "years old."
